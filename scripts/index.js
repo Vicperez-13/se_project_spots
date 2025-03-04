f6a38b1cbc02752e7327720ceb55b35391fcf8f8
Victor E. Morales Perez <vp5662021@gmail.com>
2025-03-03 21:06:44 -0800

    validation:  disable button color state

--- scripts/index.js
+++ scripts/index.js
@@ -141,10 +141,11 @@ function handleAddCardSubmit(evt) {
 profileEditButton.addEventListener("click", () => {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
-  resetValidation(editFormElement, [
-    editModalNameInput,
-    editModalDescriptionInput,
-  ]);
+  resetValidation(
+    editFormElement,
+    [editModalNameInput, editModalDescriptionInput],
+    settings
+  );
   openModal(editModal);
 });
 
